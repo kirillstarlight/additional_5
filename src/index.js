540dfdead881ef444a940e9446cfbbd5a2fcf962
kirillstarlight <kirillstarlight@gmail.com>
2018-09-11 01:33:33 +0300

    pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  stack.top = () => stack[stack.length - 1];
+
+  let bracketsMap = {};
+  bracketsConfig.forEach(([open, close]) => bracketsMap[open] = close);
+  for(var char of str) {
+      if(char === stack.top()) {
+          stack.pop();
+          continue;
+      }
+      if(!bracketsMap[char]) {
+          return false;
+      }
+      stack.push(bracketsMap[char]);
+  }
+
+  return stack.length === 0;
 }
